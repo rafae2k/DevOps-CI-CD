88444d4e2dc9c84eb42343ba8b39fcf2dbae7514
Rafael Feitoza <rafaeljr@outlook.com>
2022-04-23 19:34:48 -0300

    update coverage files

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 
 const Home: NextPage = () => {
+  const sum = (a: number, b: number) => {
+    return a + b;
+  };
+
   return (
     <>
       <Head>Hello DevOps!</Head>
       <h1>Hello DevOps</h1>
+      <div>{sum(1, 2)}</div>
     </>
   );
 };
