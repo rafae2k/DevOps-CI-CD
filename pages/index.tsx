aaa436de56912c1866b50c3ca33f1f0b0b73bcc2
Rafael Feitoza <rafaeljr@outlook.com>
2022-04-23 19:11:58 -0300

    ci: testing ci

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Head from 'next/head';
 const Home: NextPage = () => {
   return (
     <>
-      <Head>Hello DevOps</Head>
+      <Head>Hello DevOps!</Head>
       <h1>Hello DevOps</h1>
     </>
   );
