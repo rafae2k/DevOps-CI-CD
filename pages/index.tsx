056ac72d13135b4131bf808987ae531bd62f06ea
Rafael Feitoza <rafaeljr@outlook.com>
2022-04-23 10:59:09 -0300

    refactor: add h1 and remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,11 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>Hello DevOps</Head>
-      <div>Hello DevOps</div>
+      <h1>Hello DevOps</h1>
     </>
   );
 };
